4d35740d18649d9db05232c6a08e692677bad200
psk <psk3060@ebncit.co.kr>
2016-07-15 14:58:11 +0900

    Log4j => Log4j2

--- boardJsp1/src/com/common/LoggerMaster.java
+++ boardJsp1/src/com/common/LoggerMaster.java
@@ -1,33 +1,33 @@
 package com.common;
 
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class LoggerMaster {
 	public static void debug(String category, String message) {
-		Logger.getLogger(category).debug(message);
+		LogManager.getLogger(category).debug(message);
 	}
 	
 	public static void info(String category, String message) {
-		Logger.getLogger(category).info(message);
+		LogManager.getLogger(category).info(message);
 	}
 	
 	public static void trace(String category, String message) {
-		Logger.getLogger(category).trace(message);
+		LogManager.getLogger(category).trace(message);
 	}
 	
 	public static void error(String category, String message) {
-		Logger.getLogger(category).error(message);
+		LogManager.getLogger(category).error(message);
 	}
 	
 	
 	public static void fatal(String category, String message) {
-		Logger.getLogger(category).fatal(message);
+		LogManager.getLogger(category).fatal(message);
 	}
 	
 	
 	public static void warn(String category, String message) {
-		Logger.getLogger(category).warn(message);
+		LogManager.getLogger(category).warn(message);
 	}
 	
 }
