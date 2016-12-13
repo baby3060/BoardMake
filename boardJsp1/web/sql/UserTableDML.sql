8f96426ea0351b23edc023bf9a61ec649ec15c8c
psk <psk3060@ebncit.co.kr>
2016-12-13 15:59:30 +0900

    User Table DML Update

--- boardJsp1/web/sql/UserTableDML.sql
+++ boardJsp1/web/sql/UserTableDML.sql
@@ -2,6 +2,6 @@ CREATE TABLE board.user (
    user_id VARCHAR(20) NOT NULL,
    user_pass VARCHAR(20) NOT NULL,
    user_name VARCHAR(20),
-   user_sex CHAR ASCII,
+   user_gender CHAR ASCII,
    user_last_login INT
 ) ENGINE = InnoDB ROW_FORMAT = DEFAULT;
